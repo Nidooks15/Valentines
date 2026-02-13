8f73bc8a9e84d509b2b86ea07b8e91d54dcdc825
Angelmer  Pioquinto <angelmerpioquinto@gmail.com>
2026-02-14 01:34:53 +0800

    Change teddy bears to pink for better visibility

--- src/components/TeddyBears.jsx
+++ src/components/TeddyBears.jsx
@@ -8,29 +8,29 @@ const TeddyBear = ({ position, scale }) => {
       {/* Body */}
       <mesh position={[0, 0, 0]}>
         <sphereGeometry args={[0.3, 16, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       
       {/* Head */}
       <mesh position={[0, 0.4, 0]}>
         <sphereGeometry args={[0.25, 16, 16]} />
-        <meshStandardMaterial color="#A0522D" roughness={0.8} />
+        <meshStandardMaterial color="#FFC0CB" emissive="#FFC0CB" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       
       {/* Ears */}
       <mesh position={[-0.15, 0.55, 0]}>
         <sphereGeometry args={[0.1, 16, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       <mesh position={[0.15, 0.55, 0]}>
         <sphereGeometry args={[0.1, 16, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       
       {/* Snout */}
       <mesh position={[0, 0.35, 0.15]}>
         <sphereGeometry args={[0.08, 16, 16]} />
-        <meshStandardMaterial color="#D2691E" roughness={0.8} />
+        <meshStandardMaterial color="#FFE4E1" emissive="#FFE4E1" emissiveIntensity={0.1} roughness={0.6} />
       </mesh>
       
       {/* Eyes */}
@@ -52,21 +52,21 @@ const TeddyBear = ({ position, scale }) => {
       {/* Arms */}
       <mesh position={[-0.25, 0.05, 0]} rotation={[0, 0, 0.5]}>
         <capsuleGeometry args={[0.08, 0.25, 8, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       <mesh position={[0.25, 0.05, 0]} rotation={[0, 0, -0.5]}>
         <capsuleGeometry args={[0.08, 0.25, 8, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       
       {/* Legs */}
       <mesh position={[-0.12, -0.35, 0]}>
         <capsuleGeometry args={[0.1, 0.2, 8, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
       <mesh position={[0.12, -0.35, 0]}>
         <capsuleGeometry args={[0.1, 0.2, 8, 16]} />
-        <meshStandardMaterial color="#8B4513" roughness={0.8} />
+        <meshStandardMaterial color="#FFB6C1" emissive="#FFB6C1" emissiveIntensity={0.2} roughness={0.6} />
       </mesh>
     </group>
   );
